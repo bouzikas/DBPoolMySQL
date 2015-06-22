f2e0e5cd81fffa9aa7e704de35a412cb5d18cde6
Dimitris Bouzikas <dimimpou@gmail.com>
2015-06-22 15:37:04 +0300

    Added a multiple retrieve and an update routines.

--- src/main.c
+++ src/main.c
@@ -173,6 +173,42 @@ static int example_insert(int col_1, Octstr *oct_col_2)
 	return ret;
 }
 
+static int example_update(int col_1, Octstr *oct_col_2)
+{
+	Octstr *sql, *oct_col_1;
+	DBPoolConn *pconn;
+	List *binds = gwlist_create();
+	int res;
+	
+	pconn = dbpool_conn_consume(pool);
+	
+	if (pconn == NULL) {
+		return -1;
+	}
+	
+	sql = octstr_format("UPDATE `myTable` SET `col_2` = ? WHERE `col_1` = ?");
+	
+	oct_col_1 = octstr_format("%d", col_1);
+	
+	gwlist_append(binds, oct_col_2);
+	gwlist_append(binds, oct_col_1);
+	
+	debug("example", 0, "SQL: %s", octstr_get_cstr(sql));
+	
+	if ((res = dbpool_conn_update(pconn, sql, binds)) == -1)
+		error(0, "MYSQL: Error while updating entry");
+	else if (!res)
+		warning(0, "MYSQL: No entry was updated");
+	
+	dbpool_conn_produce(pconn);
+	gwlist_destroy(binds, NULL);
+	octstr_destroy(oct_col_1);
+	octstr_destroy(oct_col_2);
+	octstr_destroy(sql);
+	
+	return res;
+}
+
 static MyTableEntry *example_select_row(int col_num)
 {
 	int ret, len, i;
@@ -218,11 +254,56 @@ static MyTableEntry *example_select_row(int col_num)
 	return entry;
 }
 
+static List *example_select_rows(void)
+{
+	int ret, len, i;
+	List *result, *row;
+	List *res  = NULL;
+	DBPoolConn *pconn;
+	Octstr *sql;
+	
+	pconn = dbpool_conn_consume(pool);
+	if (pool == NULL) {
+		return NULL;
+	}
+	
+	sql = octstr_create("SELECT `col_1`, `col_2` FROM `myTable`");
+	
+	ret = dbpool_conn_select(pconn, sql, NULL, &result);
+	dbpool_conn_produce(pconn);
+	octstr_destroy(sql);
+	
+	if (ret != 0) {
+		return NULL;
+	}
+	
+	res = gwlist_create();
+	len = gwlist_len(result);
+	
+	if (len > 0) {
+		for (i = 0; i < len; i++) {
+			MyTableEntry *entry = mytable_entry_create();
+			
+			row = gwlist_extract_first(result);
+			
+			entry->col_1 = atoi(octstr_get_cstr(gwlist_get(row, 0)));
+			entry->col_2 = octstr_create(octstr_get_cstr(gwlist_get(row, 1)));
+			
+			gwlist_insert(res, i, entry);
+			gwlist_destroy(row, octstr_destroy_item);
+		}
+	}
+	gwlist_destroy(result, NULL);
+	
+	return res;
+}
 
 int main(int argc, char* argv[])
 {
 	
 	List *res;
+	MyTableEntry *entry;
+	
 	int ret, i;
 	int pool_size = 1;
 	int port = 3306;
@@ -240,15 +321,32 @@ int main(int argc, char* argv[])
 	if (pool != NULL) {
 		ret = example_create();
 		
+		// Inserting rows in table
 		for (i = 1; i < LIMIT; i++) {
 			Octstr *str = octstr_format("Text_%d", i);
 			ret = example_insert(i, str);
 			octstr_destroy(str);
 		}
 		
+		// selecting specific row using a value
 		MyTableEntry *entry = example_select_row(10);	// retrieving entry with col_1 = 10
 		if (entry != NULL) {
 			debug("example", 0, "Entry col_1: %d, col_2: %s", entry->col_1, octstr_get_cstr(entry->col_2));
+		}
+		
+		// selecting multiple rows
+		res = example_select_rows();
+		while (gwlist_len(res) > 0) {
+			entry = gwlist_consume(res);
+			debug("example", 0, "Dumping: Entry col_1: %d, col_2: %s", entry->col_1, octstr_get_cstr(entry->col_2));
+		}
+		gwlist_destroy(res, NULL);
+		
+		// update a row
+		int id = 999;
+		Octstr *new_val = octstr_create("New value");
+		if (example_update(id, new_val) == 0) {
+			debug("example", 0, "Updated col_2 with text '%s' for col_1 with value %d", octstr_get_cstr(new_val), id);
 		}
 	}
 	
